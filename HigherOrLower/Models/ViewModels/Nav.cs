360026aeac9e60eccb9da13ec765cf50266b2f2c
Nolan Williams <ntwilliams18@gmail.com>
2021-04-16 18:52:44 -0400

    Added active Nav class and reworked nav bar in _Layout to include login/logout

---
+++ HigherOrLower/Models/ViewModels/Nav.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HigherOrLower.Models
+{
+	public static class Nav
+	{
+		public static string Active(string value, string current) =>
+			(value == current) ? "active" : "";
+		public static string Active(int value, int current) =>
+			(value == current) ? "active" : "";
+	}
+}
